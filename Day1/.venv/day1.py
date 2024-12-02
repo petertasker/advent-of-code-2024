20d383a6cf62c80b85cdc9ee47fba2604e6d33d1
Peter Tasker CS2023 <peter.tasker.2023@uni.strath.ac.uk>
2024-12-02 19:44:52 +0000

    add day1 part 2

--- Day1/.venv/day1.py
+++ Day1/.venv/day1.py
@@ -2,20 +2,23 @@ def main():
 
     sum = 0
     leftLst = []
-    dct = {}
-
+    rightLst = []
     with open("input") as f:
         for line in f.readlines():
             splt = line.split()
             leftLst.append(int(splt[0]))
-            rightNum = int(splt[1])
-            dct[rightNum] = dct.get(rightNum, 0) + 1
+            rightLst.append(int(splt[1]))
+    leftLst.sort()
+    rightLst.sort()
 
-    for num in leftLst:
-        sum += num * dct.get(num, 0)
-        print(f"leftLst[i] = {num}, times this number has been seen = {dct.get(num)}, sum = {sum}")
 
-    print(f"sum = {sum}")
+    for i in range(len(leftLst)):
+        ans = (abs(leftLst[i] - rightLst[i]))
+        sum += ans
+        print(f"leftLst[i] - rightLst[i] = {ans}, sum = {sum}")
+
+    print(f"Final sum = {sum}")
+
 
 if __name__ == '__main__':
     main()
