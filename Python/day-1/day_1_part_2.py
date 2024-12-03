1472ae01456658a4daca8e20d73bd235dd9fe792
Peter Tasker CS2023 <peter.tasker.2023@uni.strath.ac.uk>
2024-12-03 18:43:30 +0000

    More appropriate variable names

--- Python/day-1/day_1_part_2.py
+++ Python/day-1/day_1_part_2.py
@@ -1,19 +1,19 @@
 def main():
     sum = 0
-    leftLst = []
+    lst = []
     dct = {}
 
     # Parse file into two lists.
     with open("input") as f:
         for line in f.readlines():
             splt = line.split()
-            leftLst.append(int(splt[0]))
-            rightNum = int(splt[1])
+            lst.append(int(splt[0]))
+            k = int(splt[1])
             # Increment dict value or set new value to 1
-            dct[rightNum] = dct.get(rightNum, 0) + 1
+            dct[k] = dct.get(k, 0) + 1
 
     # Access key value
-    for num in leftLst:
+    for num in lst:
         sum += num * dct.get(num, 0)
         print(f"leftLst[i] = {num}, times this number has been seen = {dct.get(num)}, sum = {sum}")
 
